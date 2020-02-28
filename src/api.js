081a2e7356b775d5a3c7ec37ea69af93585ab914
Xarrx <damian.h.kendzior@gmail.com>
2020-02-28 13:51:32 -0600

    Added first test

--- src/api.js
+++ src/api.js
@@ -59,30 +59,24 @@ module.exports = function(req, res){
   if(Symbol.iterator in req.body){
     // Iterate over all objects in the body object
     for(let a of req.body){
-      //console.log('DEBUG: Entered loop...');
       // check that a has an alias property and that it is in the alias module
       if(a.hasOwnProperty('alias') && alias.hasOwnProperty(a.alias)){
-        //console.log('DEBUG: Alias check passed...');
         let als = alias[a.alias];
         // validate the operation and ensure that it is allowed
-        if(a.hasOwnProperty('operation') && als.hasOwnProperty('allowed_operations')
+        if(a.hasOwnProperty('operation') 
+        && als.hasOwnProperty('allowed_operations')
         && als.allowed_operations.includes(a.operation)){
-          //console.log('DEBUG: Operation check passed...');
           // check if operation is write
-          //console.log('DEBUG: Operation: '+a.operation);
           if(a.operation === 'write'){
-            //console.log('DEBUG: Operation is write...');
             // operation is write, validate the value
             if(a.hasOwnProperty('value') && als.hasOwnProperty('validate')
             && als.validate(a.value)){
-              //console.log('DEBUG: Write check passed...');
               // requested operation has validated, add it to the queue
               queue.push({
                 pin: als.pin,
                 operation: a.operation,
                 value: a.value
               });
-              //console.log(queue);
             }else{
               // set status/message and break out of loop
               status = 500;
@@ -91,12 +85,10 @@ module.exports = function(req, res){
             }
           }else{
             // operation is something other than write so no value to validate
-            //console.log('DEBUG: Operation is not write...');
             queue.push({
               pin: als.pin,
               operation: a.operation,
             });
-            //console.log(queue);
           }
         }else{
           // set status/message and break out of loop
@@ -123,10 +115,8 @@ module.exports = function(req, res){
     /*
       do pin operations with pigpio
     */
-    //console.log(queue);
     while(queue.length){
       let a = queue.pop();
-      //console.log(a);
       if(a.operation === 'write'){
         // write value to pin
         console.log('MESSAGE: [PH] Value written to pin!');
@@ -134,7 +124,7 @@ module.exports = function(req, res){
         // read value from pin and modify resp
         console.log('MESSAGE: [PH] Value read from pin!');
       }else{
-        //console.log('DEBUG: Operation was not read or write...');
+		console.log('MESSAGE: [PH] This message should never print...');
       }
     }
   }
--- src/api.test.js
+++ src/api.test.js
@@ -1,3 +1,47 @@
 /*
-  Jest tests for api,js
+	Jest tests for api.js
+  
+	Mocks:
+	 - Need to create fake request and response objects for passing to the api 
+	   function.
+	 - Need to create a mock for axios (for when jsonstore.io is implemented).
+	 - Need to create a mock for alias.js module.
+	 
+	Notes:
+     - The for api, the expected structure of the data object is:
+	[
+		{
+			alias: <alias>,
+			operation: <read, write, etc.>,
+			[value: <value>],
+		},
+		...
+	]
 */
+'use strict';
+const api = require('./api');
+
+// response object mock
+const mockJson = jest.fn((x) => {return;});
+const mockStatus = jest.fn((x) => {return {json: mockJson};});
+const res = {status: mockStatus};
+
+// tests
+description('api Tests', () = {
+  test('Test failure on non-list (not iterable) body.', () => {
+    // api function input
+    const req = {
+      body: {}
+    };
+    // run api with constructed arguments
+    api(req, res);
+    
+    // test output for expected behavior
+    expect(mockStatus.mock.calls.length).toBe(1);
+    expect(mockStatus.mock.calls[0][0]).toBe(500);
+    expect(mockJson.mock.calls.length).toBe(1);
+    expect(mockJson).toBeCalledWith(expect.objectContaining({
+      message: 'Failed to validate request body.'
+    }));
+  });
+});
